75d205ddc8a79653f5a21d6a1417a975cd4f985d
Hanksito <barjabalseiro@gmail.com>
2022-08-02 19:09:05 +0000

    todo listo

--- src/app.py
+++ src/app.py
@@ -17,8 +17,10 @@ def add_new_todo():
     
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
-    print("This is the position to delete: ",position)
-    return 'something'
+    del todos[position]
+    return jsonify(todos)
+    
+
 
 
 if __name__ == '__main__':
